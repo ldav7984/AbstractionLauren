8cf2452cdf916339547c0771b0ab796341fefb91
Davis, Lauren <ldav7984@740s-pl-109.local>
2018-12-04 12:16:48 -0700

    Initial Commit

---
+++ AbstractionLauren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbstractionLauren
+//
+//  Created by Davis, Lauren on 12/4/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
