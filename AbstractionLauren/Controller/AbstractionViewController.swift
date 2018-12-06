10da2ce4ef4921a323367f11168fd3e20dccc62c
Davis <ldav7984@740s-pl-109.local>
2018-12-06 12:15:13 -0700

    Added scrolling methods to view controller.

--- AbstractionLauren/Controller/AbstractionViewController.swift
+++ AbstractionLauren/Controller/AbstractionViewController.swift
@@ -8,14 +8,91 @@
 
 import UIKit
 
-public class AbstractionViewController: UIPageViewController
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
 {
 
+    //MARK:- Lifecycle  methods
     public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    //MARK: -Data members
+    private lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "SwiftCode"),
+            self.newAbstractionViewController(abstractionLevel: "BlockCode"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "BinaryCode"),
+            self.newAbstractionViewController(abstractionLevel: "LogicalGate")
+        ]
+    } ()
+    
+    //MARK: Helper method to retrieve the correct ViewController based on the data member
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier:
+            "\(abstractionLevel)ViewController")
+    }
+    
+    //Mark:- Datasource required methods
+    ///Swipe left
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+                return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        return orderedAbstractionViews[previousIndex]
+    }
+    ///Swipe right
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+            else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+            else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
     }
     
 
