c06bb1bd9d9511fd820bb1305e1c324d1366a489
Davis <ldav7984@740s-pl-109.local>
2018-12-06 11:56:42 -0700

    Added view controller and images.

---
+++ AbstractionLauren/Controller/AbstractionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AbstractionViewController.swift
+//  AbstractionLauren
+//
+//  Created by Davis, Lauren on 12/6/18.
+//  Copyright © 2018 Davis, Lauren. All rights reserved.
+//
+
+import UIKit
+
+public class AbstractionViewController: UIPageViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
